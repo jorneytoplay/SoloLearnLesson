ff87f3fcf652069d2809a77cbe953614ce1b2466
jorneytoplay <jorneytoplay@gmail.com>
2021-09-30 12:26:57 +0300

    1,2,3 parts added

---
+++ SololearnPythonPractice.py
@@ -0,0 +1,29 @@
+#Practice 1.2
+print("Python is fun")
+#Practice 2.2
+print(2*68)
+#Project № 1
+print(((1*2)**30)/100)
+
+#Practice project № 2
+first=int(input())
+second=int(input())
+print(first+second)
+
+#Practice project № 3
+n = int(input())
+
+for x in range(1, n):
+    if x % 3 == 0 and x % 5 == 0:
+        print("SoloLearn")
+    elif x % 3 == 0:
+        print("Solo")
+    elif x % 5 == 0:
+        print("Learn")
+    else:
+        print(x)
+
+#Practice project № 4
+
+#Practice project № 5
+#Practice project № 6
